8db33f8d6aa3b423c3267fc05aa1e60b9dbe4af5
Sergio <ssanromangiraldez@danielcastelao.org>
2023-10-02 09:17:36 +0200

    Finalizado ejercicio 2 del boletin 2

---
+++ Boletin02/Boletin02_2/src/boletin02_2/Boletin02_2.java
@@ -0,0 +1,15 @@
+
+package boletin02_2;
+
+public class Boletin02_2 {
+
+    public static void main(String[] args) {
+        float l=3, areac;
+        areac = (float)(Math.pow(l,2));
+        System.out.println("area do cadrado="+areac+"m²");
+        //sen declarar a variable area
+        System.out.println("area2 ="+ l*l + "m²");
+        
+    }
+    
+}
