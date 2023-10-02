d30e705f29b3943f9b86b3879c64ec35c345a3f3
Sergio <ssanromangiraldez@danielcastelao.org>
2023-10-02 09:59:17 +0200

    Finalizado ejercicio 4

---
+++ Boletin02/Boletin02_4/src/boletin02_4/Boletin02_4.java
@@ -0,0 +1,24 @@
+
+package boletin02_4;
+
+import java.util.Scanner;
+
+public class Boletin02_4 {
+
+ 
+    public static void main(String[] args) {
+        double num1,num2,suma,resta,producto,cociente;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("teclea el primer numero:");
+        num1 = sc.nextDouble();
+        System.out.println("teclea el segundo numero");
+        num2 = sc.nextDouble();
+        suma= num1+num2;
+        resta= num1-num2;
+        producto= num1*num2;
+        cociente=num1/num2;
+        System.out.println("suma="+ suma +"\nresta=" + resta +"\nproducto="+producto+"\ncociente="+cociente);
+         
+    }
+    
+}
