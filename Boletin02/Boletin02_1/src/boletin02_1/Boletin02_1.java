2a8a8d9a2a5036c39a96d86bdeec7b9ce3d656fb
Sergio <ssanromangiraldez@danielcastelao.org>
2023-10-02 09:14:37 +0200

    Finalizado el ejercicio 1 del boletin 2

---
+++ Boletin02/Boletin02_1/src/boletin02_1/Boletin02_1.java
@@ -0,0 +1,12 @@
+package boletin02_1;
+
+public class Boletin02_1 {
+
+   
+    public static void main(String[] args) {
+       float base=4, altura=3,areat;
+       areat=base*altura/2;
+       System.out.println("area do triangulo="+areat+"m²");
+    }
+    
+}
