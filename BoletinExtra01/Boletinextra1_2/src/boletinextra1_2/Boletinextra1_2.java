bb4c14da432c54192f220fe23bc14e456fcbb475
Sergio <ssanromangiraldez@danielcastelao.org>
2023-10-11 11:44:50 +0200

    Finalizado el primer boletin extra

---
+++ BoletinExtra01/Boletinextra1_2/src/boletinextra1_2/Boletinextra1_2.java
@@ -0,0 +1,13 @@
+
+package boletinextra1_2;
+
+
+public class Boletinextra1_2 {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Programacion" + "\nPrimer Parcial:"+8+"\nSegundo Parcial:"+7+"\nNota final:"+7.5);
+  
+    }
+    
+}
