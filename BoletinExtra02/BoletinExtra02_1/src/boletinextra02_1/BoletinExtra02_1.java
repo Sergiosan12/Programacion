332957f9f2e4ff3b108bf121abb98de4f29ad05a
Sergio <ssanromangiraldez@danielcastelao.org>
2023-11-06 10:42:59 +0100

    Finalizado el boletin extra 2

---
+++ BoletinExtra02/BoletinExtra02_1/src/boletinextra02_1/BoletinExtra02_1.java
@@ -0,0 +1,23 @@
+
+package boletinextra02_1;
+
+import java.util.Scanner;
+
+public class BoletinExtra02_1 {
+   
+
+   
+      public static void main(String[] args) {
+        float iva=21;
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce precio: ");
+        float precio = sc.nextFloat();
+        float precioIva= precio + (precio*(iva/100));
+        System.out.print("O producto ten un prezo final de: "+precioIva+"€");
+    }
+}
+        
+    
+    
+
