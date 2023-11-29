f5e97e9d4a990b12944acab06da797a169be4a74
Sergio <ssanromangiraldez@danielcastelao.org>
2023-11-29 11:02:20 +0100

    Acabado el boletin 17

---
+++ Boletin17/Boletin17_1/src/boletin17_1/Boletin17_1.java
@@ -0,0 +1,14 @@
+
+package boletin17_1;
+
+import javax.swing.JOptionPane;
+
+public class Boletin17_1 {
+
+    public static void main(String[] args) {
+        
+       Conta cont= new Conta();
+       cont.elexirConta();    
+    }
+    
+}
