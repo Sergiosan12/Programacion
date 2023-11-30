4d13b6fce6c6947079ff237f645dbff6b13a8dea
Sergio <ssanromangiraldez@danielcastelao.org>
2023-11-30 13:19:21 +0100

    Terminado el boletin 18

---
+++ Boletin18/Boletin18_1/src/boletin18_1/Boletin18_1.java
@@ -0,0 +1,17 @@
+
+package boletin18_1;
+
+
+public class Boletin18_1 {
+
+    public static void main(String[] args) {
+        
+        
+        Chamada cha= new Chamada();
+        cha.prezoChamada();
+       
+       
+        
+    }
+    
+}
