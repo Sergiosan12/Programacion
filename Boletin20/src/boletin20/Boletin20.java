e274e1acbb07018e89410a361179949d6be0ee7b
Sergio <ssanromangiraldez@danielcastelao.org>
2024-03-01 14:05:39 +0100

    Acabado boletin 20

---
+++ Boletin20/src/boletin20/Boletin20.java
@@ -0,0 +1,16 @@
+
+package boletin20;
+
+
+public class Boletin20 {
+
+    public static void main(String[] args) {
+        
+        Canario canario = new Canario();
+        Persoa tenor= new Persoa();
+        
+        tenor.cantar();
+        canario.cantar();
+    }
+    
+}
