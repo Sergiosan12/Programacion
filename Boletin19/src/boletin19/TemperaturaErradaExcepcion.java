c60ba3a9c365e230453e1ab0d7cd360e5b8d11fd
Sergio <ssanromangiraldez@danielcastelao.org>
2024-03-01 14:01:50 +0100

    Subido boletin 19

---
+++ Boletin19/src/boletin19/TemperaturaErradaExcepcion.java
@@ -0,0 +1,13 @@
+
+package boletin19;
+
+public class TemperaturaErradaExcepcion extends Exception {
+    
+     public TemperaturaErradaExcepcion(String mensaje) {
+        super(mensaje);
+////            
+        }
+                
+    }
+    
+
